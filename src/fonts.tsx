3cd3728627699ade23be61ece2bbf61f38ef1263
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-20 15:34:56 +0900

    style: add global font

---
+++ src/fonts.tsx
@@ -0,0 +1,18 @@
+import { Global } from '@emotion/react';
+
+export default function Fonts() {
+  return (
+    <Global
+      styles={`
+        @font-face {
+            font-family: 'Pretendard-Regular';
+            src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff')
+              format('woff');
+            font-weight: 400;
+            font-style: normal;
+          }
+          
+        `}
+    />
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import Fonts from 'fonts.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+const theme = extendTheme({
+  fonts: {
+    heading: 'Pretendard',
+    body: 'Pretendard',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <Fonts />
       <App />
     </ChakraProvider>
   </React.StrictMode>
