d6091472f5578315fedb2a508d6de904e2b5e41e
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-21 22:57:42 +0900

    feat: create modal component

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,3 +1,94 @@
-export default function Modal() {
-  return <div>Modal</div>;
+import styled from '@emotion/styled';
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useCallback,
+  useMemo,
+} from 'react';
+import { createPortal } from 'react-dom';
+
+type ModalProps = {
+  width?: number;
+  height?: number;
+  title: string;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  children: ReactNode;
+};
+
+export default function Modal({
+  width = 940,
+  height = 640,
+  title,
+  setIsOpen,
+  children,
+}: ModalProps) {
+  const modalContainerStyle = useMemo(
+    () => ({
+      width,
+      height,
+    }),
+    [width, height]
+  );
+
+  const closeModal = useCallback(
+    (
+      e:
+        | React.MouseEvent<HTMLDivElement, MouseEvent>
+        | React.MouseEvent<HTMLButtonElement, MouseEvent>
+    ) => {
+      if (e.target !== e.currentTarget) return;
+      e.stopPropagation();
+      setIsOpen(false);
+    },
+    [setIsOpen]
+  );
+
+  return createPortal(
+    <BackgroundDim onClick={e => closeModal(e)}>
+      <ModalContainer
+        style={{
+          ...modalContainerStyle,
+        }}
+      >
+        <ModalTitle>{title}</ModalTitle>
+        {children}
+        <ModalCloseButton onClick={e => closeModal(e)}>X</ModalCloseButton>
+      </ModalContainer>
+    </BackgroundDim>,
+    document.body
+  );
 }
+
+const BackgroundDim = styled('div')`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(40, 40, 40, 0.2);
+`;
+
+const ModalContainer = styled('div')`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ffffff;
+  border-radius: 10px 10px 10px 10px;
+`;
+
+const ModalTitle = styled('div')`
+  font-size: 14px;
+  margin: 2rem;
+  padding-bottom: 0.4rem;
+  border-bottom: 1px solid #e7e7e7;
+`;
+
+const ModalCloseButton = styled('button')`
+  font-size: 18px;
+  position: fixed;
+  top: 2%;
+  left: 95%;
+  margin: 3px;
+`;
--- src/pages/RecordManagementPage.tsx
+++ src/pages/RecordManagementPage.tsx
@@ -1,3 +1,33 @@
+import { Button } from '@chakra-ui/react';
+import Modal from 'components/common/Modal';
+import { useState } from 'react';
+
 export default function RecordManagementPage() {
-  return <div>RecordManagementPage</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div>
+      <Button
+        onClick={() => setIsOpen(true)}
+        style={{
+          width: 146,
+          height: 44,
+          border: 'none',
+          backgroundColor: '#2D62EA',
+          color: '#FFFFFF',
+        }}
+      >
+        + 템플릿 추가
+      </Button>
+      {isOpen && (
+        <Modal
+          width={700}
+          height={400}
+          title={'템플릿 생성'}
+          setIsOpen={setIsOpen}
+        >
+          {}
+        </Modal>
+      )}
+    </div>
+  );
 }
