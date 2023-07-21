aa58a2897ed8bf8317949cbe29b95081fadc9439
Tomyo Kim <gihyun17@naver.com>
2023-07-22 00:46:51 +0900

    feat: implement youtube video hook

---
+++ src/apis/Youtube.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const API_KEY = 'API_KEY';
+
+export async function fetchYoutubeVideo(videoId:any) {
+    const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+    return (await axios.get(requestUrl)).data;
+
+}
