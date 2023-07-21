0cdaa9636ac384fc25876744d55cd3fc7bd81737
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-21 15:17:14 +0900

    chore: folder setting

---
+++ src/pages/MediaManagementPage.tsx
@@ -0,0 +1,3 @@
+export default function MediaManagementPage() {
+  return <div>MediamanagementPage</div>;
+}
---
+++ src/pages/RecordManagementPage.tsx
@@ -0,0 +1,3 @@
+export default function RecordManagementPage() {
+  return <div>RecordManagementPage</div>;
+}
