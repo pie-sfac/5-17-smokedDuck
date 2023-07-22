d3b9cbdd7330951c61ae567427ac06664be6c237
Tomyo Kim <gihyun17@naver.com>
2023-07-22 23:11:48 +0900

    fix: fix undefind error

--- src/hooks/UseYoutubeVideo.tsx
+++ src/hooks/UseYoutubeVideo.tsx
@@ -10,13 +10,19 @@ export interface YoutubeVideo {
 
 export function useYoutubeVideo () {
     const [youtubeVideo, setYoutubeVideo] = useState<YoutubeVideo|null>(null);
-    const handler =  (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handler = (event: React.ChangeEvent<HTMLInputElement>) => {
         const youtubeVideoUrl = event.target.value;
-        fetchYoutubeVideo(extractVideoIdFromUrl(youtubeVideoUrl)).then((data)=>{
+
+        if (!youtubeVideoUrl.trim()) {
+          setYoutubeVideo(null);
+          return;
+        }
+      
+        fetchYoutubeVideo(extractVideoIdFromUrl(youtubeVideoUrl))
+          .then((data) => {
             setYoutubeVideo(getYoutubeVideoFromMetedata(data.items[0]));
-        });
-        
-    }
+          });
+      };
     function extractVideoIdFromUrl(url: string) {
         const match = url.match(/(?:[?&]v=|\/embed\/|\/v\/|youtu\.be\/)([^&\n?#]+)/);
         return match ? match[1] : '';
@@ -27,9 +33,9 @@ export function useYoutubeVideo () {
 
 export function getYoutubeVideoFromMetedata (youtubeVideoMetadata:any) : YoutubeVideo {
         return {
-            'title' : youtubeVideoMetadata.snippet.title,
-            'thumbnailUrl' : youtubeVideoMetadata.snippet.thumbnails?.default?.url || '',
-            'description' : youtubeVideoMetadata.snippet.description.slice(0, 500)
+            'title' : youtubeVideoMetadata?.snippet.title,
+            'thumbnailUrl' : youtubeVideoMetadata?.snippet.thumbnails?.default?.url || '',
+            'description' : youtubeVideoMetadata?.snippet.description.slice(0, 500)
         }
 }
 
