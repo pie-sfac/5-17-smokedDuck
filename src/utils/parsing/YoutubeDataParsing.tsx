f6788d3410a7882a80aac4bd2cbf0847fda3e4bc
Tomyo Kim <gihyun17@naver.com>
2023-07-22 23:11:48 +0900

    fix: fix undefind error

--- src/utils/parsing/YoutubeDataParsing.tsx
+++ src/utils/parsing/YoutubeDataParsing.tsx
@@ -1,7 +1,5 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import React, { useEffect, useState } from 'react';
-import { useYoutubeVideo } from 'hooks/UseYoutubeVideo';
-
 
 const Container = styled.div`
   display: flex;
@@ -69,27 +67,65 @@ const ErrorMsg = styled.p`
 
 
 
-
-function LinkComponent() {
-
+function YoutubeData() {
+  const [url, setUrl] = useState('');
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const {youtubeVideo, handler} = useYoutubeVideo();
-  useEffect(()=>{
-    setDescription(youtubeVideo?.description||'');
-  },[youtubeVideo])
+  const [thumbnail, setThumbnail] = useState('');
+  const [error, setError] = useState('');
 
+
+  function handleInputChange (event: React.ChangeEvent<HTMLInputElement>) {
+    const newUrl = event.target.value;
+    setUrl(newUrl);
+    setError('');
   
-  function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
-      setTitle(event.target.value);
-  }
+    if (newUrl === '') {
+      setTitle('');
+      setDescription('');
+      setThumbnail('');
+    } else {
 
-  function handleDescriptionChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+      handleFetchInfo(newUrl);
+    }
+  };
+
+  function handleFetchInfo (newUrl: string) {
+    const videoId = extractVideoIdFromUrl(newUrl);
+
+    fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=API_KEY`)
+      .then((response) => response.json())
+      .then((data) => {
+        const item = data.items[0];
+        setTitle(item.snippet.title);
+        setThumbnail(item.snippet.thumbnails?.default?.url || '');
+        if (item.snippet.description.length > 500) {
+          setDescription(item.snippet.description.slice(0, 500));
+        } else {
+          setDescription(item.snippet.description);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setError('유효하지 않은 링크.');
+      });
+  };
+
+  function extractVideoIdFromUrl (url: string) {
+    const match = url.match(/(?:[?&]v=|\/embed\/|\/v\/|youtu\.be\/)([^&\n?#]+)/);
+    return match ? match[1] : '';
+  };
+
+  function handleDescriptionChange (event: React.ChangeEvent<HTMLTextAreaElement>) {
     const { value } = event.target;
-    console.log(value);
-      setDescription(value.slice(0, 500));
-  }
-  
+    if (value.length <= 500) {
+      setDescription(value);
+      setError('');
+    } else {
+      setError('500자까지만 입력 가능합니다.');
+    }
+  };
+
   return (
     <Container>
       <h2>센터 링크 추가</h2>
@@ -97,47 +133,45 @@ function LinkComponent() {
       <p>회원에게 발송하는 메시지에 간편하게 활용할 수 있습니다.</p>
       
       <InputTitle>링크</InputTitle>
-
       <InputWrapper>
         <Input
           type="text"
-          onChange={handler}
+          value={url}
+          onChange={handleInputChange}
           placeholder="URL을 입력해주세요"
         />
       </InputWrapper>
 
-      {youtubeVideo && (
-        youtubeVideo.thumbnailUrl && <ThumbnailBox>
-          <ThumbnailImage src={youtubeVideo.thumbnailUrl} alt="Thumbnail" />
-          <Title>{youtubeVideo.title}</Title>
+      {thumbnail && (
+        <ThumbnailBox>
+          <ThumbnailImage src={thumbnail} alt="Thumbnail" />
+          <Title>{title}</Title>
         </ThumbnailBox>
       )}
-      
-      <InputTitle>링크 제목</InputTitle>
 
+      <InputTitle>링크 제목</InputTitle>
       <Input
         type="text"
-        value={title || youtubeVideo?.title}
-        onChange={handleTitleChange}
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
         placeholder="링크 제목을 입력해 주세요."
       />
 
       <InputTitle>메모</InputTitle>
-
       <DescriptionBox>
         <DescriptionTextarea
-          value={description || youtubeVideo?.description}
+          value={description}
           onChange={handleDescriptionChange}
           placeholder="링크를 식별하기 위한 간단한 메모를 작성해 주세요. (500자 이내)"
         />
-
         <CharacterCount>
           {description.length} / 500
         </CharacterCount>
       </DescriptionBox>
 
+      {error && <ErrorMsg>{error}</ErrorMsg>}
     </Container>
   );
-}
+};
 
-export default LinkComponent;
+export default YoutubeData;
