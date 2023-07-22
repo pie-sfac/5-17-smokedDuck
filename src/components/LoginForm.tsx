e1ec6d47249863315f4192e8e6ba854874dbe15c
민정 <55127180+minjeong19@users.noreply.github.com>
2023-07-22 15:32:55 +0900

    feat: loginForm

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,191 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import axios from 'axios';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface StyledPasswordIconProps {
+  password: string;
+}
+const StyledLoginForm = styled.form`
+  display: flex;
+  position: relative;
+  top: 184px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  width: 77px;
+  height: 35px;
+  margin-bottom: 48px;
+`;
+
+const StyledUl = styled.ul`
+  display: flex;
+  margin-right: 160px;
+  margin-bottom: 24px;
+`;
+const StyledLi = styled.li`
+  padding: 0 12px;
+  list-style-type: none;
+  &:first-of-type {
+    color: #6691ff;
+    border-bottom: 2px solid #6691ff;
+  }
+  &:last-of-type {
+    color: #cfcfcf;
+    border-bottom: 1px solid #cfcfcf;
+    cursor: pointer;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 368px;
+  border: solid 1px #cfcfcf;
+  border-radius: 4px;
+  padding: 8px 16px;
+  margin-top: 4px;
+  margin-bottom: 24px;
+`;
+
+const StyledDivInput = styled.div`
+  position: relative;
+`;
+const StyledPasswordIcon = styled.img<StyledPasswordIconProps>`
+  position: absolute;
+  top: 38px;
+  right: 8px;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  display: ${props => (props.password.length !== 0 ? 'block' : 'none')};
+`;
+
+const StyledFind = styled.div`
+  color: #505050;
+  margin-right: 192px;
+  cursor: pointer;
+`;
+
+const StyledSignup = styled.div`
+  color: #505050;
+  margin-top: 94px;
+  margin-bottom: 16px;
+  & span:last-of-type {
+    cursor: pointer;
+  }
+`;
+
+const StyledLoginButton = styled.button`
+  border-radius: 4px;
+  background-color: #f4f4f4;
+  color: #aeaeae;
+  width: 312px;
+  padding: 12px 16px;
+  ${props =>
+    !props.disabled &&
+    css`
+      background-color: #2d62ea;
+      color: #fff;
+      cursor: pointer;
+    `}
+`;
+
+export default function LoginForm() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  const navigate = useNavigate();
+
+  // 로그인 정보 요청
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const basicToken = btoa(`${username}:${password}`);
+      const headers = {
+        Authorization: `Basic ${basicToken}`,
+        'Content-Type': 'application/json',
+      };
+      const data = {
+        Username: username,
+        Password: password,
+      };
+      const response = await axios.post<LoginResponse>(
+        'http://223.130.161.221/api/v1/admins/login',
+        { data },
+        { headers }
+      );
+
+      window.localStorage.setItem('token', response.data.accessToken);
+      window.localStorage.setItem('refreshToken', response.data.refreshToken);
+
+      navigate('record');
+    } catch (error) {
+      alert('아이디 또는 비밀번호가 틀렸습니다.');
+      console.error('error');
+    }
+  };
+
+  //아이디&비밀번호 입력시 버튼활성화
+  const isButtonDisabled: boolean =
+    username.length === 0 || password.length === 0;
+  const handleTogglePassword = () => {
+    setShowPassword(prevShowPassword => !prevShowPassword);
+  };
+
+  return (
+    <StyledLoginForm onSubmit={handleSubmit}>
+      <StyledImg src="" alt="" />
+      <StyledUl>
+        <StyledLi>관리자 로그인</StyledLi>
+        <StyledLi>직원 로그인</StyledLi>
+      </StyledUl>
+      <div>
+        <label htmlFor="username">아이디</label>
+        <br />
+        <StyledInput
+          type="text"
+          id="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+      </div>
+      <StyledDivInput>
+        <label htmlFor="password">비밀번호</label>
+        <br />
+        <StyledInput
+          type={showPassword ? 'text' : 'password'}
+          id="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <StyledPasswordIcon
+          src=""
+          alt=""
+          password={password}
+          onClick={handleTogglePassword}
+        />
+      </StyledDivInput>
+      <StyledFind>
+        <span>아이디 찾기</span>
+        <span> / </span>
+        <span>비밀번호 찾기</span>
+      </StyledFind>
+      <StyledSignup>
+        <span>포인티 계정이 없으세요? |</span>
+        <span>회원가입</span>
+      </StyledSignup>
+      <StyledLoginButton type="submit" disabled={isButtonDisabled}>
+        로그인
+      </StyledLoginButton>
+    </StyledLoginForm>
+  );
+}
