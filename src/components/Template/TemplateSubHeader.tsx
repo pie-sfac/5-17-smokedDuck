e968cbb57583fe833f46b2d4be3330bda3b44001
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-24 22:37:35 +0900

    feat: change template content with selected

---
+++ src/components/Template/TemplateSubHeader.tsx
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+export default function TemplateSubHeader() {
+  return (
+    <TemplateContentContainer>
+      <label htmlFor="template-title">템플릿 제목*</label>
+      <StyledInput type="text" name="template-title" id="template-title" />
+      <br />
+      <label htmlFor="template-title">설명</label>
+      <StyledInput type="text" name="template-title" id="template-title" />
+    </TemplateContentContainer>
+  );
+}
+
+const TemplateContentContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem;
+  font-size: 0.8rem;
+`;
+
+const StyledInput = styled('input')`
+  border-bottom: 1px solid #e7e7e7;
+  font-size: 0.9rem;
+  margin: 0.2rem;
+`;
