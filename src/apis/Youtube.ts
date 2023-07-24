d85ae294306ac4953698f40c264da53c7252d6d8
Tomyo Kim <gihyun17@naver.com>
2023-07-24 17:40:21 +0900

    feat: add View page

--- src/apis/Youtube.ts
+++ src/apis/Youtube.ts
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 
-const API_KEY = 'API_KEY';
+const API_KEY = 'AIzaSyBg1mCHyMITB1NjtW3EJDGR_MFIac0zX20';
 
-export async function fetchYoutubeVideo(videoId:any) {
-    const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
-    return (await axios.get(requestUrl)).data;
-
+export async function fetchYoutubeVideo(videoId: any) {
+  const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+  return (await axios.get(requestUrl)).data;
 }
