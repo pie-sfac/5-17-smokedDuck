e2ac00886f9b552b67d119163de15f1b6dcb16fa
Lee-SeungHoon <vheh0901@gmail.com>
2023-07-24 13:38:21 +0900

    feat: record template list html/css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import styled from '@emotion/styled';
 import AppRouter from 'Router';
 
 function App() {
   return (
-    <div>
-      piehealthcare project!
+    <AppContainer>
       <AppRouter />
-    </div>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  width: 1440px;
+  height: 100vh;
+  margin: 0 auto;
+`;
 
 export default App;
