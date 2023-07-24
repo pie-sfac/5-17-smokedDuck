b1e093f5bdc7ff95e2c070924e1deddb5b981b47
Tomyo Kim <gihyun17@naver.com>
2023-07-24 13:26:16 +0900

    feat: add create component into modal

--- src/apis/Youtube.ts
+++ src/apis/Youtube.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'API_KEY';
+const API_KEY = 'MyApiKey';
 
 export async function fetchYoutubeVideo(videoId:any) {
     const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
