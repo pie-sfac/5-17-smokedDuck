234719e2ea76a9ddcf8942239cc62f51e8fb342c
민정 <55127180+minjeong19@users.noreply.github.com>
2023-07-24 23:30:09 +0900

    feat: add categoryTab

--- src/pages/MediaManagementPage.tsx
+++ src/pages/MediaManagementPage.tsx
@@ -1,44 +1,115 @@
-import { Button } from '@chakra-ui/react';
+import { Button, chakra } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { useState } from 'react';
 
 import Modal from '@/components/common/Modal';
-import LinkComponent from '@/components/Link';
+import MediaCard from '@/components/MediaCard';
+import { categoryList } from '@/utils/constants/categoryList';
+import { mediaList } from '@/utils/constants/mediaList';
 
 export default function MediaManagementPage() {
   const [isOpen, setIsOpen] = useState(false);
-  return (
-    <div>
-      <Button
-        onClick={() => setIsOpen(true)}
-        style={{
-          width: 146,
-          height: 44,
-          border: 'none',
-          backgroundColor: '#2D62EA',
-          color: '#FFFFFF',
-        }}
-      >
-        + 링크 추가
-      </Button>
+  const [selectedCategory, setSelectedCategory] = useState(categoryList[0]?.id);
 
-      {isOpen && (
-        <Modal
-          width={1064}
-          height={736}
-          title={'센터 링크 추가'}
-          setIsOpen={setIsOpen}
-        >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-            }}
+  const handleCategoryClick = (categoryId: number) => {
+    setSelectedCategory(categoryId);
+  };
+
+  return (
+    <MediaListContainer>
+      <CategoryTitle>
+        {categoryList.map(item => (
+          <ul key={item.id}>
+            <li
+              onClick={() => handleCategoryClick(item.id)}
+              style={{
+                color: selectedCategory === item.id ? '#6691FF' : 'inherit',
+                borderBottom:
+                  selectedCategory === item.id
+                    ? '1px solid #6691FF'
+                    : 'inherit',
+              }}
+            >
+              {item.title}
+            </li>
+          </ul>
+        ))}
+        <EditButton>
+          <button>편집</button>
+        </EditButton>
+      </CategoryTitle>
+      <ListBackGround>
+        {mediaList.map(item => (
+          <MediaCard
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+        <BlueButton onClick={() => setIsOpen(true)}>+ 링크 추가</BlueButton>
+        {isOpen && (
+          <Modal
+            width={700}
+            height={400}
+            title={'템플릿 생성'}
+            setIsOpen={setIsOpen}
           >
-            <LinkComponent />
-          </div>
-        </Modal>
-      )}
-    </div>
+            {}
+          </Modal>
+        )}
+      </ListBackGround>
+    </MediaListContainer>
   );
 }
+
+const MediaListContainer = styled('div')`
+  margin: 6rem auto 0 auto;
+`;
+
+const ListBackGround = styled('div')`
+  display: flex;
+  position: relative;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  margin: 1rem;
+  width: 1408px;
+  height: 624px;
+  padding: 16px;
+  background-color: rgba(235, 241, 255, 0.8);
+  border-radius: 16px;
+`;
+
+const BlueButton = chakra(Button, {
+  baseStyle: {
+    width: '146px',
+    height: '44px',
+    border: 'none',
+    background: '#2d62ea',
+    color: '#ffffff',
+    position: 'absolute',
+    bottom: '-3.5rem',
+    right: '0',
+  },
+});
+
+const CategoryTitle = styled('div')`
+  display: flex;
+  position: relative;
+  margin-top: 2rem;
+  & > ul {
+    list-style: none;
+    cursor: pointer;
+    padding: 0 1rem;
+  }
+`;
+
+const EditButton = styled('div')`
+  position: absolute;
+  right: 1rem;
+  width: 64px;
+  height: 32px;
+  text-align: center;
+  padding-top: 4px;
+  border: 1px solid #e7e7e7;
+  border-radius: 8px;
+`;
---
+++ src/utils/constants/categoryList.ts
@@ -0,0 +1,20 @@
+type categoryListType = {
+  id: number;
+  title: string;
+};
+
+export const categoryList: categoryListType[] = [
+  { id: 0, title: '전체' },
+  {
+    id: 1,
+    title: '복합 관절 운동',
+  },
+  {
+    id: 2,
+    title: '단순 관절 운동',
+  },
+  {
+    id: 3,
+    title: '특수 운동',
+  },
+];
