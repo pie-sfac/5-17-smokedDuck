fbf100f56385625d39380ee6f5d2183b543f7732
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-24 23:44:59 +0900

    feat: question selections

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -63,9 +63,7 @@ export default function Modal({
         <TemplateContent
           selectedTemplateTitle={selectedTemplateTitle}
           setSelectedTemplateTitle={setSelectedTemplateTitle}
-        >
-          {}
-        </TemplateContent>
+        />
         <ModalCloseButton onClick={e => closeModal(e)}>X</ModalCloseButton>
       </ModalContainer>
     </BackgroundDim>,
