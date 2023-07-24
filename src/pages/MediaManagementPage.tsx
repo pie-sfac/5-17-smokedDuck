709820da76ebe69ccdb5832227c528ae7da252a2
민정 <55127180+minjeong19@users.noreply.github.com>
2023-07-24 13:33:29 +0900

    feat: create media template

--- src/pages/MediaManagementPage.tsx
+++ src/pages/MediaManagementPage.tsx
@@ -1,3 +1,24 @@
+import styled from '@emotion/styled';
+import MediaList from 'components/MediaList';
+
+const MediaListContainer = styled('div')`
+  display: flex;
+  position: fixed;
+  top: 24%;
+  left: 14%;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  width: 1400px;
+  height: 624px;
+  padding: 16px;
+  background-color: rgba(235, 241, 255, 0.26);
+  border-radius: 16px;
+`;
+
 export default function MediaManagementPage() {
-  return <div>MediamanagementPage</div>;
+  return (
+    <MediaListContainer>
+      <MediaList />
+    </MediaListContainer>
+  );
 }
