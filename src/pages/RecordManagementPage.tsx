c171316fb999f9ee0517811f13c05b68717e00d1
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-24 15:12:43 +0900

    feat: create selection box

--- src/pages/RecordManagementPage.tsx
+++ src/pages/RecordManagementPage.tsx
@@ -1,7 +1,10 @@
 import { Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { useState } from 'react';
 
+import Template from '@/assets/Template.svg';
 import Modal from '@/components/common/Modal';
+import SelectionBox from '@/components/common/SelectionBox';
 
 export default function RecordManagementPage() {
   const [isOpen, setIsOpen] = useState(false);
@@ -26,9 +29,30 @@ export default function RecordManagementPage() {
           title={'템플릿 생성'}
           setIsOpen={setIsOpen}
         >
-          {}
+          {
+            <SelectionBoxContainer>
+              <SelectionBox
+                title={'문진 템플릿'}
+                titleDescription={'첫 방문 또는 회원 현재 상태를 체크 합니다.'}
+                image={Template}
+              />
+
+              <SelectionBox
+                title={'처치 템플릿'}
+                titleDescription={'수업 시, 작성합니다.'}
+                image={Template}
+              />
+            </SelectionBoxContainer>
+          }
         </Modal>
       )}
     </div>
   );
 }
+
+const SelectionBoxContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 16rem;
+`;
