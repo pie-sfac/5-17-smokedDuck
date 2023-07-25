51dceb713f109ee634536dad9619e703b7a7b452
Tomyo Kim <gihyun17@naver.com>
2023-07-25 16:14:22 +0900

    fix: add env file

--- src/apis/Youtube.ts
+++ src/apis/Youtube.ts
@@ -2,9 +2,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyBg1mCHyMITB1NjtW3EJDGR_MFIac0zX20';
-
 export async function fetchYoutubeVideo(videoId: any) {
-  const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${API_KEY}`;
+  const requestUrl = `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoId}&key=${
+    import.meta.env.VITE_APP_GA_API_KEY
+  }`;
   return (await axios.get(requestUrl)).data;
 }
