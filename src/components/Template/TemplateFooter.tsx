88286becd25b32eb34feb1cb572141461ee5fcc6
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-25 10:56:14 +0900

    feat: create template footer

---
+++ src/components/Template/TemplateFooter.tsx
@@ -0,0 +1,31 @@
+import { Button } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+type TemplateFooterProps = {
+  questions: string[];
+};
+
+export default function TemplateFooter({ questions }: TemplateFooterProps) {
+  return (
+    <FooterContainer>
+      <Button
+        fontSize={'1rem'}
+        _active={{
+          bg: '#6691FF',
+          color: '#F2F0F0',
+        }}
+        borderRadius={'20px 20px 20px 20px'}
+        isDisabled={questions.length < 1 ? true : false}
+      >
+        저장
+      </Button>
+    </FooterContainer>
+  );
+}
+
+const FooterContainer = styled('div')`
+  height: 3.7rem;
+  margin: 0.2rem 2rem 0.2rem 2rem;
+  display: flex;
+  justify-content: flex-end;
+`;
