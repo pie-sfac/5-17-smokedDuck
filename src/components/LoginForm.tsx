f46708688b8a63637b3ddc171a7e404ab5d2f37e
Lee-SeungHoon <vheh0901@gmail.com>
2023-07-26 15:36:55 +0900

    refactor: change style code position

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -16,93 +16,6 @@ interface StyledPasswordIconProps {
   password: string;
 }
 
-const StyledLoginForm = styled.form`
-  display: flex;
-  position: relative;
-  top: 184px;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const StyledImg = styled.img`
-  width: 77px;
-  height: 35px;
-  margin-bottom: 48px;
-`;
-
-const StyledUl = styled.ul`
-  display: flex;
-  margin-right: 160px;
-  margin-bottom: 24px;
-`;
-
-const StyledLi = styled.li`
-  padding: 0 12px;
-  list-style-type: none;
-  &:first-of-type {
-    color: #6691ff;
-    border-bottom: 2px solid #6691ff;
-  }
-  &:last-of-type {
-    color: #cfcfcf;
-    border-bottom: 1px solid #cfcfcf;
-    cursor: pointer;
-  }
-`;
-
-const StyledInput = styled.input`
-  width: 368px;
-  border: solid 1px #cfcfcf;
-  border-radius: 4px;
-  padding: 8px 16px;
-  margin-top: 4px;
-  margin-bottom: 24px;
-`;
-
-const StyledDivInput = styled.div`
-  position: relative;
-`;
-
-const StyledPasswordIcon = styled.img<StyledPasswordIconProps>`
-  position: absolute;
-  top: 38px;
-  right: 8px;
-  width: 24px;
-  height: 24px;
-  cursor: pointer;
-  display: ${props => (props.password.length !== 0 ? 'block' : 'none')};
-`;
-
-const StyledFind = styled.div`
-  color: #505050;
-  margin-right: 192px;
-  cursor: pointer;
-`;
-
-const StyledSignup = styled.div`
-  color: #505050;
-  margin-top: 94px;
-  margin-bottom: 16px;
-  & span:last-of-type {
-    cursor: pointer;
-  }
-`;
-
-const StyledLoginButton = styled.button`
-  border-radius: 4px;
-  background-color: #f4f4f4;
-  color: #aeaeae;
-  width: 312px;
-  padding: 12px 16px;
-  ${props =>
-    !props.disabled &&
-    css`
-      background-color: #2d62ea;
-      color: #fff;
-      cursor: pointer;
-    `}
-`;
-
 export default function LoginForm() {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
@@ -195,3 +108,90 @@ export default function LoginForm() {
     </StyledLoginForm>
   );
 }
+
+const StyledLoginForm = styled.form`
+  display: flex;
+  position: relative;
+  top: 184px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledImg = styled.img`
+  width: 77px;
+  height: 35px;
+  margin-bottom: 48px;
+`;
+
+const StyledUl = styled.ul`
+  display: flex;
+  margin-right: 160px;
+  margin-bottom: 24px;
+`;
+
+const StyledLi = styled.li`
+  padding: 0 12px;
+  list-style-type: none;
+  &:first-of-type {
+    color: #6691ff;
+    border-bottom: 2px solid #6691ff;
+  }
+  &:last-of-type {
+    color: #cfcfcf;
+    border-bottom: 1px solid #cfcfcf;
+    cursor: pointer;
+  }
+`;
+
+const StyledInput = styled.input`
+  width: 368px;
+  border: solid 1px #cfcfcf;
+  border-radius: 4px;
+  padding: 8px 16px;
+  margin-top: 4px;
+  margin-bottom: 24px;
+`;
+
+const StyledDivInput = styled.div`
+  position: relative;
+`;
+
+const StyledPasswordIcon = styled.img<StyledPasswordIconProps>`
+  position: absolute;
+  top: 38px;
+  right: 8px;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
+  display: ${props => (props.password.length !== 0 ? 'block' : 'none')};
+`;
+
+const StyledFind = styled.div`
+  color: #505050;
+  margin-right: 192px;
+  cursor: pointer;
+`;
+
+const StyledSignup = styled.div`
+  color: #505050;
+  margin-top: 94px;
+  margin-bottom: 16px;
+  & span:last-of-type {
+    cursor: pointer;
+  }
+`;
+
+const StyledLoginButton = styled.button`
+  border-radius: 4px;
+  background-color: #f4f4f4;
+  color: #aeaeae;
+  width: 312px;
+  padding: 12px 16px;
+  ${props =>
+    !props.disabled &&
+    css`
+      background-color: #2d62ea;
+      color: #fff;
+      cursor: pointer;
+    `}
+`;
