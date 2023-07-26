fa5d001c4642c5b2668f9d086836f90296855c88
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-26 13:22:54 +0900

    feat: create question component

---
+++ src/components/Question/QuestionContent.tsx
@@ -0,0 +1,3 @@
+export default function QuestionContent() {
+  return <div>Question Content</div>;
+}
---
+++ src/components/Question/QuestionHeader.tsx
@@ -0,0 +1,3 @@
+export default function QuestionHeader() {
+  return <div>Question Header</div>;
+}
