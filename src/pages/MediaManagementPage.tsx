ca6f1b5887ee24432baa5ad6eb0cd35316899fef
Lee-SeungHoon <vheh0901@gmail.com>
2023-07-26 15:37:51 +0900

    refactor: change Folder Name

--- src/pages/MediaManagementPage.tsx
+++ src/pages/MediaManagementPage.tsx
@@ -3,7 +3,8 @@ import { useContext } from 'react';
 
 import Category from '@/components/Category';
 import Modal from '@/components/Common/Modal';
-import LinkComponent from '@/components/Link/index';
+import LinkComponent from '@/components/Link';
+import CategorySelector from '@/components/Media/CategorySelector';
 import MediaListContainer from '@/components/Media/MediaListContainer';
 import { MainContext } from '@/store';
 
