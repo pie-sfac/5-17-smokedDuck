298ac244ef5d4e702cc204f12d619401a05e10d1
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-26 10:00:07 +0900

    feat: add question selections

--- src/components/Template/TemplateQuestionSelections.tsx
+++ src/components/Template/TemplateQuestionSelections.tsx
@@ -1,5 +1,15 @@
 import { Box } from '@chakra-ui/react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
+
+import Condition from '@/assets/Condition.svg';
+import Media from '@/assets/Media.svg';
+import Pain from '@/assets/Pain.svg';
+import PainQuestion from '@/assets/PainQuestion.svg';
+import Selections from '@/assets/Selections.svg';
+import Text from '@/assets/Text.svg';
+
+import QuestionBox from '../common/QuestionBox';
 
 type TemplateQuestionSelectionsProps = {
   setQuestions: React.Dispatch<React.SetStateAction<string[]>>;
@@ -8,6 +18,7 @@ type TemplateQuestionSelectionsProps = {
 export default function TemplateQuestionSelections({
   setQuestions,
 }: TemplateQuestionSelectionsProps) {
+  const [selectedQuestion, setSelectedQuestion] = useState('');
   return (
     <div>
       <QeustionsContainer>
@@ -25,7 +36,7 @@ export default function TemplateQuestionSelections({
             color: '#FFFFFF',
           }}
           marginRight={2}
-          onClick={() => setQuestions(['temporary'])}
+          onClick={() => setSelectedQuestion('basic')}
         >
           + 기본 문항
         </Box>
@@ -43,10 +54,67 @@ export default function TemplateQuestionSelections({
             borderColor: '#1FB881',
             color: '#FFFFFF',
           }}
+          onClick={() => setSelectedQuestion('specialty')}
         >
           + 전문 문항
         </Box>
       </QeustionsContainer>
+      {selectedQuestion === 'basic' && (
+        <QuestionBoxContainer>
+          <QuestionBox
+            image={Text}
+            title={'텍스트'}
+            description={'텍스트 형식의 답변을 입력하는 문항입니다.'}
+            tagName={'기본'}
+            margin={'0.4rem'}
+            onClick={setQuestions}
+          />
+          <QuestionBox
+            image={Media}
+            title={'미디어'}
+            description={'이미지 혹은 영상을 답변으로 첨부하는 문항입니다. '}
+            tagName={'기본'}
+            margin={'0.4rem'}
+            onClick={setQuestions}
+          />
+          <QuestionBox
+            image={Selections}
+            title={'선택형'}
+            description={'보기 중 선택해서 답변하는 문항입니다.'}
+            tagName={'기본'}
+            onClick={setQuestions}
+          />
+        </QuestionBoxContainer>
+      )}
+      {selectedQuestion === 'specialty' && (
+        <QuestionBoxContainer>
+          <QuestionBox
+            image={Pain}
+            title={'통증 정도'}
+            description={'회원의 통증 정도를 선택하는 문항입니다.'}
+            tagName={'전문'}
+            margin={'0.4rem'}
+            onClick={setQuestions}
+          />
+          <QuestionBox
+            image={Condition}
+            title={'오늘의 컨디션'}
+            description={'회원의 컨디션 정도를 선택하는 문항입니다.'}
+            tagName={'전문'}
+            margin={'0.4rem'}
+            onClick={setQuestions}
+          />
+          <QuestionBox
+            image={PainQuestion}
+            title={'통증 문진'}
+            description={
+              '통증 부위, 유형, 정도, 빈도, 기간을 작성할 수 있는 문항입니다.'
+            }
+            tagName={'전문'}
+            onClick={setQuestions}
+          />
+        </QuestionBoxContainer>
+      )}
     </div>
   );
 }
@@ -55,4 +123,10 @@ const QeustionsContainer = styled('div')`
   display: flex;
   justify-content: flex-end;
   margin: 0 2rem 1rem 2rem;
+`;
+
+const QuestionBoxContainer = styled('div')`
+  display: flex;
+  margin: 0 2rem 0 2rem;
+  justify-content: space-between;
 `;
