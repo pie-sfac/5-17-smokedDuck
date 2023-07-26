c4cc9630725b5265eb017fe30f8b467808fe99f4
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-27 00:15:49 +0900

    feat: create question component (header)

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,13 +1,33 @@
+import styled from '@emotion/styled';
+
 import QuestionContent from './QuestionContent';
 import QuestionFooter from './QuestionFooter';
 import QuestionHeader from './QuestionHeader';
 
-export default function Question() {
+interface QuestionProps {
+  _id: number;
+  title: string;
+  tagName: string;
+}
+
+export default function Question({ _id, title, tagName }: QuestionProps) {
   return (
-    <div>
-      <QuestionHeader />
+    <QuestionContainer>
+      <QuestionHeader id={_id} title={title} tagName={tagName} />
       <QuestionContent />
       <QuestionFooter />
-    </div>
+    </QuestionContainer>
   );
 }
+
+const QuestionContainer = styled('div')`
+  width: 96%;
+  display: flex;
+  background-color: #ffffff;
+  border: 1px solid #e7e7e7;
+  border-radius: 10px 10px 10px 10px;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 1rem;
+  margin: 1rem 1rem 0 1rem;
+`;
