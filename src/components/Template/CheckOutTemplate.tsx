ac1ca085b61e8fd2b9fd5f021fdd9f0bc50443f6
Lee-SeungHoon <vheh0901@gmail.com>
2023-07-27 14:14:16 +0900

    fix: delete unnecessary title

--- src/components/Template/CheckOutTemplate.tsx
+++ src/components/Template/CheckOutTemplate.tsx
@@ -1,13 +1,11 @@
 import TemplateSelectedQuestionContainer from './TemplateSelectedQuestionContainer';
 import TemplateSubHeader from './TemplateSubHeader';
-import TemplateTitle from './TemplateTitle';
 
 export default function CheckOutRecordTemplate() {
   return (
     <>
-      <TemplateTitle />
       <TemplateSubHeader />
-      <TemplateSelectedQuestionContainer questions={[]} />
+      <TemplateSelectedQuestionContainer />
     </>
   );
 }
