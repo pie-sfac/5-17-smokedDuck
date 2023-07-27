dcd6dfbd302ddcac43039d88ce685d817fb2ccc0
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-07-27 16:03:51 +0900

    feat: create media detail content attatchment

--- src/components/Question/QuestionContent.tsx
+++ src/components/Question/QuestionContent.tsx
@@ -1,10 +1,19 @@
 import styled from '@emotion/styled';
+import { ChangeEvent, useCallback, useState } from 'react';
 
 type QuestionContentProps = {
   title: string;
 };
 
 export default function QuestionContent({ title }: QuestionContentProps) {
+  const [addedFile, setAddedFile] = useState<File>();
+
+  const handleAddedFile = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== undefined && e.target.files !== null) {
+      setAddedFile(e.target.files[0]);
+    }
+  }, []);
+
   return (
     <QuestionContentContainer>
       <StyledLabel htmlFor="questionTitle">문항 제목</StyledLabel>
@@ -28,7 +37,28 @@ export default function QuestionContent({ title }: QuestionContentProps) {
           margin: '0.4rem 0 0.4rem 0',
         }}
       />
-      {title === '미디어' && <div>+ 답변자 미디어 첨부</div>}
+      {title === '미디어' && (
+        <AddMediaContainer>
+          <AddMediaButton>
+            <label htmlFor="mediaFile">+ 답변자 미디어 첨부</label>
+            <input
+              type="file"
+              name="mediaFile"
+              id="mediaFile"
+              onChange={e => handleAddedFile(e)}
+              style={{ display: 'none' }}
+            />
+          </AddMediaButton>
+          {addedFile !== undefined && (
+            <div>
+              <ChangeMediaButton onClick={() => setAddedFile(undefined)}>
+                파일 변경
+              </ChangeMediaButton>
+              {addedFile?.name}
+            </div>
+          )}
+        </AddMediaContainer>
+      )}
       {title === '선택형' && <div>보기 추가</div>}
     </QuestionContentContainer>
   );
@@ -65,4 +95,35 @@ const StyledTextArea = styled('textarea')`
     color: #cfcfcf;
     font-size: 0.8rem;
   }
+`;
+
+const AddMediaContainer = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+
+const AddMediaButton = styled('div')`
+  width: 18rem;
+  height: 2.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.8rem;
+  font-weight: normal;
+  background-color: rgba(244, 244, 244, 0.5);
+  border: 1px dashed #cfcfcf;
+  border-radius: 10px 10px 10px 10px;
+  margin: 0.5rem 0.5rem 0.5rem 0;
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const ChangeMediaButton = styled('button')`
+  background-color: #ebf1ff;
+  font-weight: normal;
+  font-size: 0.5rem;
+  margin-right: 0.4rem;
+  padding: 0.3rem;
+  border-radius: 10px 10px 10px 10px;
 `;
