1ad14519a7e37341ba7125cf209ea1a809391310
Lee-SeungHoon <vheh0901@gmail.com>
2023-08-01 20:29:56 +0900

    feat: create record detail template

--- src/types/recordDetail.interface.ts
+++ src/types/recordDetail.interface.ts
@@ -3,20 +3,18 @@ export interface recordDetailType {
   category: string;
   title: string;
   description: 'string';
-  questions: [
-    {
-      id: number;
-      type: string;
-      order: number;
-      required: boolean;
-      title: string;
-      description: string;
-      paragraph: boolean;
-      options: string[];
-      allowMultiple: boolean;
-      addOtherOption: boolean;
-    },
-  ];
+  questions: {
+    id: number;
+    type: string;
+    order: number;
+    required: boolean;
+    title: string;
+    description: string;
+    paragraph: boolean;
+    options: string[];
+    allowMultiple: boolean;
+    addOtherOption: boolean;
+  }[];
   createdAt: string;
   updatedAt: string;
   message: string;
