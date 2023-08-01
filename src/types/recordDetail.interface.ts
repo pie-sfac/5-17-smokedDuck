7e628d5dcf7a1ca1ba995c76163c42864d637a87
Lee-SeungHoon <vheh0901@gmail.com>
2023-08-01 17:24:27 +0900

    feat: make useRecordDetail Hook for api connect

---
+++ src/types/recordDetail.interface.ts
@@ -0,0 +1,23 @@
+export interface recordDetailType {
+  id: number;
+  category: string;
+  title: string;
+  description: 'string';
+  questions: [
+    {
+      id: number;
+      type: string;
+      order: number;
+      required: boolean;
+      title: string;
+      description: string;
+      paragraph: boolean;
+      options: string[];
+      allowMultiple: boolean;
+      addOtherOption: boolean;
+    },
+  ];
+  createdAt: string;
+  updatedAt: string;
+  message: string;
+}
