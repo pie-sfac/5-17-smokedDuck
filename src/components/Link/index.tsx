94f6f7507935c59aa64c15dae9e3760170f2eb0c
Tomyo Kim <gihyun17@naver.com>
2023-08-03 01:57:50 +0900

    style: make space

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -14,6 +14,7 @@ import {
   GetLinkDetailResponse,
   GetLinkListResponse,
 } from '@/types/media.interface';
+
 export interface LinkComponentProps {
   mode: 'CREATE' | 'UPDATE';
   linkId?: number;
