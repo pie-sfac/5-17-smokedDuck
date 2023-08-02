5c66004ba523fb1ff03d1035af2527b1c30223eb
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-02 11:32:09 +0900

    feat: add template, question interface

--- src/types/question.interface.ts
+++ src/types/question.interface.ts
@@ -1,7 +1,21 @@
-export interface Question {
-  _id: number;
-  questionTitle: string;
+export interface Questions {
+  type:
+    | ''
+    | 'TEXT'
+    | 'MEDIA'
+    | 'SELECT'
+    | 'PAIN_HSTRY'
+    | 'CONDITION'
+    | 'PAIN_INTV';
+  order: number;
+  title: string;
   tagName: string;
+  description?: string;
+  required: boolean;
+  paragraph?: boolean;
+  options?: string[];
+  allowMultiple?: boolean;
+  addOtherOption?: boolean;
 }
 
 export interface AddedFile {
