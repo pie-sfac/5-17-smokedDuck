26970f96353f613617fcf7a48f66afc317072483
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-02 11:32:09 +0900

    feat: add template, question interface

--- src/types/template.interface.ts
+++ src/types/template.interface.ts
@@ -1,21 +1,8 @@
+import { Questions } from './question.interface';
+
 export interface Template {
-  category: 'INTERVIEW' | 'TREATMENT';
+  category: '' | 'INTERVIEW' | 'TREATMENT';
   title: string;
   description?: string;
-  question: {
-    type:
-      | 'TEXT'
-      | 'MEDIA'
-      | 'SELECT'
-      | 'PAIN_HSTRY'
-      | 'CONDITION'
-      | 'PAIN_INTV';
-    order: number;
-    description?: string;
-    required: boolean;
-    paragraph?: boolean;
-    options?: string[];
-    allowMultiple?: boolean;
-    addOtherOption?: boolean;
-  };
+  question: Questions[];
 }
