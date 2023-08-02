2d92d42442112e8fde7486fe91d5323efeaea90b
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-02 11:35:36 +0900

    feat: add question, template context

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -46,7 +46,6 @@ export const MainContext = React.createContext<ContextType>({
   recordModalOpen: false,
   mediaModalOpen: false,
   selectedTemplateTitle: '',
-
   questions: {
     type: '',
     order: 0,
