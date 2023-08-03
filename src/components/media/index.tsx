773c8decaa6c20489fb01a4fed1ca6d5f4a9f4bf
민정 <55127180+minjeong19@users.noreply.github.com>
2023-08-03 18:02:32 +0900

    refactor: separate Media Components

---
+++ src/components/media/index.tsx
@@ -0,0 +1,11 @@
+import MediaCategorySelector from './MediaCategorySelector';
+import MediaListContainer from './MediaListContainer';
+
+export default function Media() {
+  return (
+    <>
+      <MediaCategorySelector />
+      <MediaListContainer />
+    </>
+  );
+}
