757ceb604b2b8c1b1e566b0dd39321c40db9e94b
민정 <55127180+minjeong19@users.noreply.github.com>
2023-08-04 14:03:44 +0900

    refactor: MediaModalOpen Main page, move to index

--- src/components/media/index.tsx
+++ src/components/media/index.tsx
@@ -1,11 +1,37 @@
+import { useContext } from 'react';
+
+import { MainContext } from '@/store';
+
+import Modal from '../Common/Modal';
+import LinkComponent from '../Link';
 import MediaCategorySelector from './MediaCategorySelector';
 import MediaListContainer from './MediaListContainer';
 
 export default function Media() {
+  const { mediaModalOpen, setMediaModalState } = useContext(MainContext);
+
   return (
     <>
       <MediaCategorySelector />
       <MediaListContainer />
+      {mediaModalOpen && (
+        <Modal
+          width={940}
+          height={640}
+          title={'센터 링크 생성'}
+          setIsOpen={setMediaModalState}
+        >
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              height: '100%',
+            }}
+          >
+            <LinkComponent mode="CREATE" />
+          </div>
+        </Modal>
+      )}
     </>
   );
 }
--- src/pages/MediaManagementPage.tsx
+++ src/pages/MediaManagementPage.tsx
@@ -1,35 +1,11 @@
 import styled from '@emotion/styled';
-import { useContext } from 'react';
 
-import Modal from '@/components/Common/Modal';
-import LinkComponent from '@/components/Link';
 import Media from '@/components/Media';
-import { MainContext } from '@/store';
 
 export default function MediaManagementPage() {
-  const { mediaModalOpen, setMediaModalState } = useContext(MainContext);
-
   return (
     <MediaContainer>
       <Media />
-      {mediaModalOpen && (
-        <Modal
-          width={940}
-          height={640}
-          title={'센터 링크 생성'}
-          setIsOpen={setMediaModalState}
-        >
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            <LinkComponent mode="CREATE" />
-          </div>
-        </Modal>
-      )}
     </MediaContainer>
   );
 }
