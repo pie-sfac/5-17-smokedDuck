92e6ad13a92c4ac70f2bd00cdca558c8fca14786
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-08 15:38:52 +0900

    add select question rules

--- src/components/Template/index.tsx
+++ src/components/Template/index.tsx
@@ -42,6 +42,23 @@ export default function Template() {
   };
 
   const handleClickedSaveButton = async () => {
+    questionList.map(question => {
+      if (question.type === 'SELECT') {
+        if (question.options?.length === 0) {
+          alert('선택형 문항의 보기가 존재하지 않습니다.');
+          return;
+        } else {
+          const set = new Set(question.options);
+          if (set.size !== question.options?.length) {
+            alert(
+              '작성하신 선택형 문항의 보기 중 중복값이 존재합니다. 중복을 수정해주세요.'
+            );
+            return;
+          }
+        }
+      }
+    });
+
     const newTemplateContent: NewTemplateContent = {
       ...templateContent,
       questions: questionList,
