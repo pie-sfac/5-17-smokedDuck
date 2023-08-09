4394ae0509b1f7e99ef0e14b4a22a09a14779b01
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-09 15:24:26 +0900

    refactor: apply usecallback hook, divide constants

---
+++ src/utils/constants/template.ts
@@ -0,0 +1,7 @@
+export const templateNotificationText = {
+  untitledTemplate: '템플릿 제목을 입력해주세요.',
+  noOptions: '선택형 문항의 보기가 존재하지 않습니다.',
+  untitledQuestion: '문항의 제목을 입력해주세요.',
+  duplicateOptions:
+    '작성하신 선택형 문항의 보기 중 중복값이 존재합니다. 중복을 수정해주세요.',
+};
