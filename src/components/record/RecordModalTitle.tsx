05c96befa575aa9f724ea5d97f468acaca4647dd
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-09 17:44:06 +0900

    refactor: combine DeleteModal component logic

---
+++ src/components/record/RecordModalTitle.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+
+import useRecordDetail from '@/hooks/useRecordDetail';
+
+import Loading from '../Common/Loading';
+
+type RecordModalHeaderPropType = {
+  id: number;
+};
+
+export default function RecordModalHeader({ id }: RecordModalHeaderPropType) {
+  const { recordDetailData } = useRecordDetail(id);
+
+  if (!recordDetailData) {
+    return (
+      <TemplateContentContainer>
+        <Loading />
+      </TemplateContentContainer>
+    );
+  }
+
+  return (
+    <TemplateContentContainer>
+      <label htmlFor="template-title">템플릿 제목*</label>
+      <StyledHedaerText>{recordDetailData.title}</StyledHedaerText>
+      <br />
+      <label htmlFor="template-title">설명</label>
+      <StyledHedaerText>{recordDetailData.description}</StyledHedaerText>
+    </TemplateContentContainer>
+  );
+}
+
+const TemplateContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 2rem 2rem 1rem 2rem;
+  font-size: 0.8rem;
+`;
+
+const StyledHedaerText = styled.div`
+  border-bottom: 1px solid #e7e7e7;
+  font-size: 0.9rem;
+  margin: 0.2rem;
+  padding: 0 0.2rem 0 0.2rem;
+`;
